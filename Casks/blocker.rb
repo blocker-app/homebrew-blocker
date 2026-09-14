2b3f81a11c048d234437ab9d4acdea11c6fe5e45
Léo Mathurin <leo.mathurin@epitech.eu>
2026-09-14 17:41:37 +0200

    chore: update Homebrew references after organization transfer

--- Casks/blocker.rb
+++ Casks/blocker.rb
@@ -2,8 +2,8 @@ cask "blocker" do
   version "1.1.0"
   sha256 "9f937c82b9df384182062ffe682adfec0a7adde9afff04a7e113080237c34d46"
 
-  url "https://github.com/leo-mathurin/blocker-releases/releases/download/v#{version}/blocker-#{version}.dmg",
-      verified: "github.com/leo-mathurin/blocker-releases/"
+  url "https://github.com/blocker-app/blocker-releases/releases/download/v#{version}/blocker-#{version}.dmg",
+      verified: "github.com/blocker-app/blocker-releases/"
   name "Blocker"
   desc "Minimal website blocker with recurring schedules"
   homepage "https://getblocker.app"
